281a3fb82246b20ac377c6888c2f348947b7da17
Alejandro Castillo <alejandrocastillochaves@gmail.com>
2011-10-23 12:54:12 -0400

    changes to donate

--- secure/admin/eventsavevalidators.js
+++ secure/admin/eventsavevalidators.js
@@ -1,3 +1,5 @@
+
+
 function Validator(theForm) {
 	if (!theForm.EventName.value.length || trim(theForm.EventName.value) == "") {
                 alert("Please enter a value for the \"Event Name\" field.");
@@ -6,3 +8,19 @@ function Validator(theForm) {
             }
     return true;        
 }
+
+function trim(inputStringTrim) {
+            fixedTrim = "";
+            lastCh = " ";
+            for (x = 0; x < inputStringTrim.length; x++) {
+                ch = inputStringTrim.charAt(x);
+                if ((ch != " ") || (lastCh != " ")) {
+                    fixedTrim += ch;
+                }
+                lastCh = ch;
+            }
+            if (fixedTrim.charAt(fixedTrim.length - 1) == " ") {
+                fixedTrim = fixedTrim.substring(0, fixedTrim.length - 1);
+            }
+            return fixedTrim;
+}    
