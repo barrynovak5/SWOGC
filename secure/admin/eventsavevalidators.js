c65d103fa733b95735e9e1a483bf3dc0da1783f1
sgentile <steven.gentile@gmail.com>
2011-10-22 22:43:16 -0400

    updates

---
+++ secure/admin/eventsavevalidators.js
@@ -0,0 +1,8 @@
+function Validator(theForm) {
+	EventName
+	if (!theForm.EventName.value.length || trim(theForm.EventName.value) == "") {
+                alert("Please enter a value for the \"Event Name\" field.");
+                theForm.EventName.focus();
+                return (false);
+            }
+}
